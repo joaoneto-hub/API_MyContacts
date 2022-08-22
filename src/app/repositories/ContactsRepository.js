0c5b287722a7bde0c60c1a3d767e83174fb19300
joaoneto-hub <joaorodrigues8182@gmail.com>
2022-08-22 17:35:50 -0300

    Rotas delete e show

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,20 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'João Rodrigues',
     email: 'joaoteles8182@gmail.com',
     phone: '123456',
-    category: uuid(),
+    category: v4(),
+
+  },
+  {
+    id: v4(),
+    name: 'Rafaela Rodrigues',
+    email: 'Rafaelateles8182@gmail.com',
+    phone: '6543456',
+    category: v4(),
 
   },
 ];
@@ -15,6 +23,19 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
