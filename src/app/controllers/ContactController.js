99e6184f70bd277660b5611daa9afd2171b92fd5
joaoneto-hub <joaorodrigues8182@gmail.com>
2022-08-22 22:29:15 -0300

    Rota de Criar contato

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -21,8 +21,27 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
     // Criar um registro
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(404).json({ error: 'Name is required' });
+    }
+
+    const contactExist = await ContactsRepository.findByEmail(email);
+
+    if (contactExist) {
+      return response.status(404).json({ error: 'This e-mail is already been taken' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   update() {
